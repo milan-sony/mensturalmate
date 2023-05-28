ac1d7c58383b574177430e37640e0518d229da50
Milan Sony <milanfrom2001@gmail.com>
2023-05-29 00:39:11 +0530

    structure created, function to add period details added

--- periods_tracker.c
+++ periods_tracker.c
@@ -2,50 +2,69 @@
 #include <conio.h>
 #include <stdlib.h>
 
-void add_details(){
-    // system("cls");
-    printf("add");
+// stucture to hold the period details
+struct period_details
+{
+    int day;
+    int month;
+    int year;
+    int cycle_length;
+    int period_length;
+}periods;
+
+// function to add period details
+void add_period_details(){
+    printf("\n\n\tADD DETAILS\n\n");
+    printf("\tWHEN DID YOUR LAST PERIOD START ?\n");
+    printf("\n\tDAY(DD): ");
+    scanf("%d", &periods.day);
+    printf("\n\tMONTH(MM): ");
+    scanf("%d", &periods.month);
+    printf("\n\tYEAR(YYYY): ");
+    scanf("%d", &periods.year);
+    printf("\n\tYOUR PERIOD DETAILS ADDED SUCCESSFULLY (PRESS ANY KEY TO CONTINUE)");
     getch();
 }
 
-void track_periods(){
-    // system("cls");
-    printf("track");
+// function to view period details
+void view_period_details(){
+    printf("view");
     getch();
 }
 
-void edit_details(){
-    // system("cls");
+// function to edit period details
+void edit_period_details(){
     printf("edit");
     getch();
 }
 
+// main function
 void main(){
-    int ch;
+    int choice;
     printf("\n\n\t-----------------------------------------\n");
     printf("\t\t\tMENSTURA\n");
     printf("\t-----------------------------------------");
     while (1)
     {
         printf("\n\n\t\t\tMAIN MENU");
-        printf("\n\n\t\tADD DETAILS\t[ENTER-1]");
-        printf("\n\n\t\tTRACK PERIODS\t[ENTER-2]");
-        printf("\n\n\t\tEDIT DETAILS\t[ENTER-3]");
-        printf("\n\n\t\t\tEXIT\t[ENTER-4]");
+        printf("\n\n\t\tADD DETAILS\t[PRESS-1]");
+        printf("\n\n\t\tVIEW DETAILS\t[PRESS-2]");
+        printf("\n\n\t\tEDIT DETAILS\t[PRESS-3]");
+        printf("\n\n\t\t\tEXIT\t[PRESS-0]");
         printf("\n\n\t=> ENTER YOUR CHOICE: ");
-        scanf("%d", &ch);
-        switch (ch)
+        scanf("%d", &choice);
+        switch (choice)
         {
             case 1:
-                add_details();
+                add_period_details();
                 break;
             case 2:
-                track_periods();
+                view_period_details();
                 break;
             case 3:
-                edit_details();
+                edit_period_details();
                 break;
-            case 4:
+            case 0:
                 exit(0);
             default:
                 printf("\n\tYOU HAVE ENTERED WRONG CHOICE");
